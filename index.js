139d948e55d730ddf8a25a9183d7a257ec67da66
Michael Barajas <michael.a.barajas@gmail.com>
2015-10-30 09:49:39 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,91 @@
+var bluebird = require('bluebird');
+var request = require('request');
+
+function IsLeaked(servers, owaspConfig) {
+    if (typeof servers === 'object' && servers !== null) {
+        this.servers = servers;
+    } else {
+        this.servers = require('./servers.json').servers;
+    }
+
+    if (typeof owaspConfig === 'object' && owaspConfig !== null) {
+        this.owaspConfig = owaspConfig;
+    } else {
+        this.owaspConfig = null;
+    }
+}
+
+/**
+ * Returns a servers ip address to be used when performing API requests
+ * @return {String} - The IP address in string form
+ */
+IsLeaked.prototype.getWeightedServer = function() {
+    var len = this.servers.length;
+    var check = Math.random();
+    for(var i = 0; i < len; i++) {
+        var server = this.servers[i];
+        if (server.active === true && server.weight >= check) {
+            return server.ip;
+        }
+    }
+    // Remove weight check to prevent race condition for weight only existing on offline servers
+    for(var i = 0; i < len; i++) {
+        var server = this.servers[i];
+        if (server.active === true) {
+            return server.ip;
+        }
+    }
+
+    throw new Error("No servers are available to proccess your request.");
+}
+
+/**
+ * Checks for the existence of a password in a know password list.
+ * @param  {string}   password The password to verify against isLeaked
+ * @param  {Function} cb       Ca;; back function(err, result)
+ */
+IsLeaked.prototype.isLeakedPassword = function(password, cb) {
+    return request({
+        url: this.getWeightedServer() + "/password/isLeaked",
+        method: "POST",
+        json: true,
+        body: { password: password }
+    }, function(err, res, body) {
+        if (res.statusCode !== 200) {
+            return cb(body, null);
+        }
+
+        return cb(null, body.isLeaked === true);
+    });
+}
+
+/**
+ * Tests the password against OWASP and isLeaked, allowing you to consolidate your password verification
+ * @param  {string}   password The password to verify against isLeaked
+ * @param  {[object]}   owaspConfig OWASP config as defined at https://www.npmjs.com/package/owasp-password-strength-test
+ * @param  {Function} cb   callback function (err, body), where body is the type returned at
+ *                                https://www.npmjs.com/package/owasp-password-strength-test
+ */
+IsLeaked.prototype.testPassword = function(password, cb) {
+    var body = {
+        password: password
+    }
+
+    if(this.owaspConfig !== null) {
+        body.config = this.owaspConfig;
+    }
+
+    return request({
+        url: this.getWeightedServer() + "/password/test",
+        method: "POST",
+        json: true,
+        body: body
+    }, function(err, res, body) {
+        if (res.statusCode !== 200) {
+            return cb(body, null);
+        }
+        return cb(null, body);
+    });
+}
+
+module.exports = IsLeaked;
